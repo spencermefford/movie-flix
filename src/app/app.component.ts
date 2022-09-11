195ab53650f26aaf7aa3d897396dc210a19da78d
Spencer Mefford <spencermefford@gmail.com>
2022-09-11 15:50:29 -0600

    Handle genres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Apollo, gql } from 'apollo-angular';
 
 @Component({
@@ -9,13 +10,22 @@ import { Apollo, gql } from 'apollo-angular';
 export class AppComponent {
   title = 'movie-flix';
   genres = [];
+  currentGenre = '';
   loading= true;
   error:any;
-  isCollapsed = true;
+  isCollapsed = false;
 
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // Get Query Param
+    this.route.queryParams
+      .subscribe(({genre}) => {
+        this.currentGenre = genre;
+      }
+    );
+
+    // Fetch Genres
     this.apollo
       .watchQuery({
         query: gql`
@@ -29,5 +39,13 @@ export class AppComponent {
         this.loading = result.loading;
         this.error = result.error;
       });
+  }
+
+  setGenre(genre: string): void {
+    this.router.navigate(
+      ['/'],
+      { queryParams: { genre } }
+    );
+
   }
 }
