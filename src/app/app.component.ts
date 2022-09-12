03493714dc5f3abe09b769364737c51907a101f3
Spencer Mefford <spencermefford@gmail.com>
2022-09-11 21:26:48 -0600

    Show/hide sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.startsWith('/movie')) {
+          this.isCollapsed = true;
+        } else if (event.url === '/') {
+          this.isCollapsed = false;
+        }
+      }
+    });
+  }
 }
